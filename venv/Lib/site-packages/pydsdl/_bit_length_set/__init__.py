cf44b4f5d5618ac7c09e1c9ffc76020f9627f8cf
Alina Zhu <alinazhu02@gmail.com>
2026-02-10 13:55:02 -0500

    Remove venv from repo

--- venv/Lib/site-packages/pydsdl/_bit_length_set/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2021 OpenCyphal
-# This software is distributed under the terms of the MIT License.
-# Author: Pavel Kirienko <pavel@opencyphal.org>
-
-from ._bit_length_set import BitLengthSet as BitLengthSet
