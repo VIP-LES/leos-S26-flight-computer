4fc0ea381f56b61446b0b7921858d3f7f6eb39d1
Simgoo <vineethpateel@gmail.com>
2026-04-17 19:54:11 -0400

    startup script path updates

--- startup.sh
+++ startup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
-cd /opt/leos-S26-flight-computer
-source .venv/bin/activate
+REPO="/home/leos-flight-computer/leos-S26-flight-computer"
+
+cd "$REPO"
+source "$REPO/.venv/bin/activate"
+
+# Only needed if DSDL imports ever fail
+export PYTHONPATH="$REPO/dsdl_out:$PYTHONPATH"
+
 exec python -m fc.services.logger
